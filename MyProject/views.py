ca432c91c6bf7950670137775834808c7ad556b4
Chinmaydg98 <chiinmaydgawande1998@gmail.com>
2021-12-03 18:48:39 +0530

    'Free form calculator' prototype implemented

--- MyProject/views.py
+++ MyProject/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
     """Homepage"""
     return render(request, 'index.html')
+
+
+def calculate(request):
+    """Calculation"""
+    expression = request.GET.get('inExpression', '')
+
+    try:
+        result = eval(expression)
+    except SyntaxError:
+        return HttpResponse("Syntax Error. Please input a valid expression")
+    except ZeroDivisionError:
+        return HttpResponse("Division by zero")
+    else:
+        return HttpResponse(result)
