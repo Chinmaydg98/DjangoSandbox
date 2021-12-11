c75d38f2ae2a3b21dee4c4a43f239464649e3b48
Chinmaydg98 <chiinmaydgawande1998@gmail.com>
2021-12-11 19:48:32 +0530

    1. Created Model: 'Journal' 2. Registered model 3. Created migration for Journal model

--- journals_app/models.py
+++ journals_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Journal(models.Model):
+    """
+    A Journal holds entries, which in turn hold topics
+    Hierarchy is as follows:
+        JOURNAL -(contain)-> ENTRIES -(contain)-> TOPICS
+    """
+    title = models.TextField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Simply returns name of Journal"""
+        return str(self.title).upper()
