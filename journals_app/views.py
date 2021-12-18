75e964044700f5c72c0a83982db70271c4807f0f
Chinmaydg98 <chiinmaydgawande1998@gmail.com>
2021-12-18 13:12:27 +0530

    Fixed import lines

--- journals_app/views.py
+++ journals_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import *
-from .forms import *
+from .models import Journal, Entry
+from .forms import JournalForm, EntryForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
